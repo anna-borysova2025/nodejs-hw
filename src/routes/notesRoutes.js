bc6293f62e0de4422ebf31f9616cd07867468114
anna_borysova <anna@pavlyuk.org>
2026-03-29 11:55:05 +0300

    upd

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,19 +1,25 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { noteIdParamSchema, updateNoteSchema, getAllNotesSchema } from '../validations/notesValidation.js';
-import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
-
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote,
+} from '../controllers/notesController.js';
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
-router.get('/notes',getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
-router.get('/notes/noteId', celebrate(noteIdParamSchema), getNoteById);
-router.delete('/notes/noteId', celebrate(noteIdParamSchema), deleteNote);
-router.patch('/notes/noteId', celebrate(updateNoteSchema), updateNote);
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
