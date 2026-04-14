872b0f46564df1c0e8d44ee03fc4fe6fa2f58d4b
anna_borysova <anna@pavlyuk.org>
2026-04-14 20:35:45 +0300

    feat: implement password reset functionality with email notifications and user avatar upload

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,6 +23,7 @@ app.use(helmet());
 
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
